596f0f9c8cf7f7474f3e7d2e856066fa0414f91e
leunj <leunj87511@gmail.com>
2022-06-22 16:34:48 +0900

    SpringBoot 설정완료

---
+++ src/main/java/moa/moamore/MoamoreApplication.java
@@ -0,0 +1,13 @@
+package moa.moamore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoamoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoamoreApplication.class, args);
+	}
+
+}
